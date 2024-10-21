90cfdb30727cb0c237b28dad4db7fd4d736ef141
salma22022 <salmahammouda2002@gmail.com>
2024-10-21 16:05:42 +0300

    test2

--- obj/Debug/net8.0/Project.AssemblyInfo.cs
+++ obj/Debug/net8.0/Project.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Project")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a36f9848b0c5ae40ab9e4636ab5e9ebd70725320")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+90736bfbbb24755b2ef6e89d534bc56f8074a88b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Project")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Project")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
